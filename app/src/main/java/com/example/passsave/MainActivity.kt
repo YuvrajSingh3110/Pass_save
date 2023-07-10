b556f4b5484e11c5e6f715542c698f8ebc270ebc
Yuvraj Singh <ysrishu1031@gmail.com>
2023-07-10 20:27:19 +0530

    adding ui

--- app/src/main/java/com/example/passsave/MainActivity.kt
+++ app/src/main/java/com/example/passsave/MainActivity.kt
@@ -3,21 +3,39 @@ package com.example.passsave
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
 import com.example.passsave.ui.theme.PassSaveTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            PassDatabase::class.java,
+            "pass.db"
+        ).build()
+    }
+
+    private val viewModel by viewModels<PassViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory{
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return PassViewModel(db.dao) as T
+                }
+            }
+        }
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PassSaveTheme {
-
+                val state by viewModel.state.collectAsState()
+                PassScreen(state = state, onEvent = viewModel::onEvent)
             }
         }
     }
--- app/src/main/java/com/example/passsave/PassViewModel.kt
+++ app/src/main/java/com/example/passsave/PassViewModel.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
